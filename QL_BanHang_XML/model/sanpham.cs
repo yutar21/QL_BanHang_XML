a3f4098acb93ad8d08bf93758ba7d8bc9e3f13ba
Yutar <100964305+yutar21@users.noreply.github.com>
2023-12-24 20:32:22 +0700

    model sanpham

---
+++ QL_BanHang_XML/model/sanpham.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_BanHang_XML.model
+{
+    class sanpham
+    {
+        public String id {  get; set; }
+        public String name { get; set; }
+        public String gia { get; set; }
+        public String soluong { get; set; }
+        public String mota { get; set; }
+        public String ngaysanxuat { get; set; }
+        public String hansudung { get; set; }
+
+
+
+
+    }
+}
