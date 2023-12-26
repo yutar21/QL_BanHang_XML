0456a1e346ddea95182d362f8fa09bff54ece2e4
DanJacker <125642292+DanJacker@users.noreply.github.com>
2023-12-26 07:53:17 +0700

    add class khachhang

---
+++ QL_BanHang_XML/model/khachhang.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_BanHang_XML.model
+{
+    internal class khachhang
+    {
+    }
+}
